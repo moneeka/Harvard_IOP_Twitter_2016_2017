859f74cef38c0ce1241ab1ded700a774e6a8cee5
Monica Mishra <monicamishra@college.harvard.edu>
2016-02-20 13:35:58 -0500

    Cleaning up the file naming

--- tweet_divider.py
+++ tweet_divider.py
@@ -2,22 +2,28 @@ import json
 
 class tweet_divider():
 	try:
+		# Creates the list that will contain the objects contained in the json file 
 		data = []
-		with open('nevada.json') as data_file: 
+		with open('data.json') as data_file: 
 
 			for line in data_file:
+				# converts each line into a json object
 				data.append(json.loads(line))
 		data_file.close()
 
-		with open('coordinatePartion.json', 'w') as partition1_file:
-			with open('placePartion.json', 'w') as partition2_file:
+		# Opens up the empty file that will store the tweets with actual coordinates
+		with open('coordinatePartition.json', 'w') as partition1_file:
+			# Opens up the empty file that will store the tweets with place and null coordinates
+			with open('placePartition.json', 'w') as partition2_file:
 
 				for s in data:
+					# Checks to see if the coordinates field is filled in
 					if s["coordinates"] != None:
-	
+						# Writes the json version of tweet into the appropriate file
 						partition1_file.write(json.dumps(s))
 						partition1_file.write("\n")
 					else:
+						# Writes the json version of tweet that just has the place into the appropriate file
 						partition2_file.write(json.dumps(s))
 						partition2_file.write("\n")
 			partition2_file.close()
