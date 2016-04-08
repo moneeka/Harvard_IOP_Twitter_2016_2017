f960c5a65ca232a78873fafa500845606c960028
cinnamolly <cinnamon@college.harvard.edu>
2016-04-08 17:52:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
 import tweet_divider
-import tweet_cleaner
+import tweet_cleaner2
 import sort
 
 def main(argv):
 
 	state = argv[0]
 	dataFile = argv[1]
+	
+	dateIndex = dataFile.find("2016")
+	date = dataFile[dateIndex:]
+	
+	stateFile = "tweets_" + state + "_" + date
+	coordinateFile = "coordinatePartition_" + state + "_" + date
+	dictionaryFile = "dictionary_" + state + "_" + date
+	
+	tweet_divider.tweet_divider(dataFile, coordinateFile, 'placePartion.json')
+	#eliminate non-political tweets
+	tweet_cleaner2.tweet_cleaner2(coordinateFile, stateFile, state)
+	sort.sort(coordinateFile, dictionaryFile, state)
 
-	stateFile = state + "_tweets.json"
-
-	tweet_divider.tweet_divider(dataFile, 'coordinatePartition.json', 'placePartion.json')
-	tweet_cleaner.tweet_cleaner('coordinatePartition.json', stateFile, state)
-	sort.sort('coordinatePartition.json', 'dictionary.json', state)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
