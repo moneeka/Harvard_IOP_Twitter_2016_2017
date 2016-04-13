5e4150142b68981e48993bdafd87084c7b960a61
cinnamolly <cinnamon@college.harvard.edu>
2016-04-13 16:52:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,17 @@ def main(argv):
 	stateFile = "tweets_" + state + "_" + date
 	coordinateFile = "coordinatePartition_" + state + "_" + date
 	dictionaryFile = "dictionary_" + state + "_" + date
-
-	tweet_divider.tweet_divider(dataFile, coordinateFile, 'placePartition.json')
+	partitionFile = "placePartition_" + state + "_" + date
+	
+	tweet_divider.tweet_divider(dataFile, coordinateFile, partitionFile)
 	#eliminate non-political tweets
-	tweet_cleaner2.tweet_cleaner2(coordinateFile, stateFile, state)
-	return sort.sort(coordinateFile, dictionaryFile, state)
+	tweet_cleaner2.tweet_cleaner2(coordinateFile, stateFile, state, True)
+	print "coordinates"
+	sort.sort(coordinateFile, dictionaryFile, state, True)
+	print "place"
+	tweet_cleaner2.tweet_cleaner2(partitionFile, stateFile, state, False)
+	sort.sort(partitionFile, dictionaryFile, state, False)
+
 
 
 if __name__ == "__main__":
