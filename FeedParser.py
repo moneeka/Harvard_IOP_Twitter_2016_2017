0b5d9678088b92d6ce97633fe67984ab61b0e73f
Sam Plank <samuelplank@college.harvard.edu>
2016-09-23 06:36:27 -0400

    TextBlob sentiment analysis

--- FeedParser.py
+++ FeedParser.py
@@ -2,54 +2,64 @@ import feedparser
 import json
 
 f = open('nyt.txt', 'w')
+articles = []
 
 d = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')
 length = len(d['entries'])
 for entry in d['entries']:
 	article = {'Title': entry['title'], 'PubDate': entry.published}
-	json.dump(article, f)
+	articles.append(article)
+json.dump(articles, f)
 f.close()
 
 
 f2 = open('wsj.txt', 'w')
+articles = []
 
 d = feedparser.parse('http://www.wsj.com/xml/rss/3_7085.xml')
 length = len(d['entries'])
 for entry in d['entries']:
 	article = {'Title': entry['title'], 'PubDate': entry.published}
-	json.dump(article, f2)
+	articles.append(article)
+json.dump(articles, f2)
 f2.close()
 
 f3 = open('breitbart.txt', 'w')
+articles = []
 
 d = feedparser.parse('http://feeds.feedburner.com/breitbart?format=xml')
 length = len(d['entries'])
 for entry in d['entries']:
 	title = (entry['title']).encode('ascii', 'ignore')
 	article = {'Title': title, 'PubDate': entry.published}
-	json.dump(article, f3)
+	articles.append(article)
+json.dump(articles, f3)
 f3.close()
 
 
 f4 = open('fox.txt', 'w')
+articles = []
 
 d = feedparser.parse('http://feeds.foxnews.com/foxnews/latest?format=xml')
 length = len(d['entries'])
 for entry in d['entries']:
 	title = (entry['title']).encode('ascii', 'ignore')
 	article = {'Title': title, 'PubDate': entry.published}
-	json.dump(article, f4)
+	articles.append(article)
+json.dump(articles, f4)
 f4.close()
 
 
 f5 = open('cnn.txt', 'w')
+articles = []
 
 d = feedparser.parse('http://rss.cnn.com/rss/cnn_topstories.rss')
 length = len(d['entries'])
 for entry in d['entries']:
 	title = (entry['title']).encode('ascii', 'ignore')
 	article = {'Title': title, 'PubDate': entry.published}
-	json.dump(article, f5)
+	articles.append(article)
+json.dump(articles, f5)
 f5.close()
 
 
