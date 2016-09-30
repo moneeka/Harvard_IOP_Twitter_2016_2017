4eb179c1f5a8c0f18b8cfd2a2e40327325d061cf
Sam Plank <samuelplank@college.harvard.edu>
2016-09-30 12:44:03 -0400

    sentiment for each

--- blobsent.py
+++ blobsent.py
@@ -2,24 +2,33 @@ from urllib2 import urlopen
 import json
 from textblob import TextBlob
 
-with open('fox.txt') as inputfile:
-	data = json.load(inputfile)
-inputfile.close()
 
-sent_added = []
-
-for article in data:
-	title = TextBlob(article['Title'])
-
-	sentiment = title.sentiment.polarity
-	subjectivity = title.sentiment.subjectivity
+names = ['nyt.txt','wsj.txt','breitbart.txt','fox.txt','cnn.txt']
 
 
-	piece = {'Title': article['Title'], 'PubDate': article['PubDate'], 'Sentiment': sentiment, 'Subjectivity': subjectivity}
-	sent_added.append(piece)
+def sentize(publication_name):
+        with open('fox.txt') as inputfile:
+                data = json.load(inputfile)
+        inputfile.close()
+
+        sent_added = []
+
+        for article in data:
+                title = TextBlob(article['Title'])
+
+                sentiment = title.sentiment.polarity
+                subjectivity = title.sentiment.subjectivity
 
 
-f = open('fox.txt', 'w')
-json.dump(sent_added, f)
-f.close()
+                piece = {'Title': article['Title'], 'PubDate': article['PubDate'], 'Sentiment': sentiment, 'Subjectivity': subjectivity}
+                sent_added.append(piece)
 
+
+        f = open('fox.txt', 'w')
+        json.dump(sent_added, f)
+        f.close()
+
+
+
+for name in names:
+        sentize(name)
