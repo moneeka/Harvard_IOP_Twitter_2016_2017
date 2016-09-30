c7214f54b9fe4c43f4a322390156c4a23ef392f3
Monica Mishra <Monica Mishra>
2016-09-30 14:24:12 -0400

    pushing twitter scraping files again

---
+++ twitter_hashtag_filter.py
@@ -0,0 +1,42 @@
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+consumer_key = "#"
+consumer_secret = "#"
+access_token = "#"
+access_secret = "#"
+        
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            with open('hashtag_filtered_tweets.json', 'a') as f:
+                f.write("HINIFND")
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+data = ''
+with open('political_hashtags_for_twitter.txt', 'r') as data_file: 
+
+    for line in data_file:
+        data += ', ' + line
+data_file.close()
+
+
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=[data])
