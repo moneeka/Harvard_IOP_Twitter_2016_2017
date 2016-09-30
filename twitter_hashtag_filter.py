2e9c47c6665778604756a10ea156580f78782c58
Monica Mishra <Monica Mishra>
2016-09-30 15:14:52 -0400

    added tweet data pulled from hashtags. stored in the hashtag_data_files folder

--- twitter_hashtag_filter.py
+++ twitter_hashtag_filter.py
@@ -3,10 +3,15 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-consumer_key = "#"
-consumer_secret = "#"
-access_token = "#"
-access_secret = "#"
+# consumer_key = "#"
+# consumer_secret = "#"
+# access_token = "#"
+# access_secret = "#"
+
+consumer_key = "WUkXAAr4Fr8fnwEWztJC4bZcA"
+consumer_secret = "eT7D2y5EuKkmmYmKQWXrF8Sazc9qpHuts0fL3hfaoGcO3MpSEU"
+access_token = "781660569132789760-3LR6EC39U2KGQR0ECa9m75MbI8F0t0y"
+access_secret = "lrR3gMRIwjbSpHjfjfvMRMvnTcWwy0U1k6OAJWdgvQjAv"
         
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -30,7 +35,7 @@ class MyListener(StreamListener):
         return True
 
 data = ''
-with open('political_hashtags_for_twitter.txt', 'r') as data_file: 
+with open('hashtag_data_files\political_hashtags_for_twitter.txt', 'r') as data_file: 
 
     for line in data_file:
         data += ', ' + line
