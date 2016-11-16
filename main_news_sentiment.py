8821d36fca736526d1f067e0594170de71bbe604
Sam Plank <samuelplank@college.harvard.edu>
2016-11-16 13:55:19 -0500

    wsj counts

--- main_news_sentiment.py
+++ main_news_sentiment.py
@@ -7,9 +7,9 @@ def main(argv):
     data_file = argv[0]
     source = argv[1]
 
-    output_file = source + '_political_news_sentimentized_file.txt'
+    output_file = source + '_nonpolitical_news_sentimentized_file.txt'
     
-    # NewsSent_Molly.news_sent(data_file, output_file)
+    NewsSent_Molly.news_sent(data_file, output_file)
     return sentiment_percentage_Monica.sentiment_percentage(output_file)
 
 if __name__ == "__main__":
