d4f2a0a514b1793ac99a4f34233799598b164645
Sam Plank <samuelplank@college.harvard.edu>
2017-03-03 03:28:28 -0500

    count and graph phone number tweets

--- TweetSent.py
+++ TweetSent.py
@@ -6,16 +6,7 @@ import json
 import time
 import os
 
-def tweet_sent(data_file):
-    tweet_files = []
-    with open(data_file) as data_file:
-        for line in data_file:
-            if line != None:
-                tweet_files.append(line.rstrip('\n'))
-    data_file.close()
-
-    names = tweet_files
-
+def sentize(file_name):
 
     pro_conservative_w = ['#makeamericagreatagain', '#imwithyou', '#maga', '#trump2016', '#trumptrain', '#defenddonald', '#defendthesecond', '#onenationundergod',
     '#righttobeararms', '#donttreadonme', '#prolife', '#progod', '#progun', '#gunrights', '#latinosfortrump', '#gaysfortrump', '#votetrump', '#alllivesmatter',
@@ -26,60 +17,59 @@ def tweet_sent(data_file):
     anti_liberal_w = ['#libtards','#liberallogic','#nobama','#stophillary','#nohillary','#notreadyforhillary','#neverhillary','#toosavagefordemocrats'
     '#hillaryforprison2016', '#hillaryforprison', '#fuckhillary', '#neverhillary', '#hillno', '#imnotwithher', '#clintonnewsnetwork', '#hillaryforprison2016',
     '#lockherup', '#crookedhillary']
+
     with open('tweets_sentiment_count.csv', 'a') as outputfile:
-       outputfile.write('Pro Liberal, Pro Conservative, Anti Liberal, Anti Conservative, Date\n')
-    def sentize(file_name):
-        with open(file_name) as inputfile:
-            data = json.load(inputfile)
-        inputfile.close()
-        
+        outputfile.write('Pro Liberal, Pro Conservative, Anti Liberal, Anti Conservative, Date\n')
 
-        pro_conservative = 0
-        pro_liberal = 0
-        pro_3rd = 0
-        anti_conservative = 0
-        anti_liberal = 0
-        total = 0
+    with open(file_name) as inputfile:
+        data = json.load(inputfile)
+    inputfile.close()
+    
 
-        txt_val = file_name.index('_cleaned')
-        pure_file_name = file_name[:txt_val]
-        date_val = file_name.index('2016')
-        date = pure_file_name[date_val:]
-        print date
+    pro_conservative = 0
+    pro_liberal = 0
+    pro_3rd = 0
+    anti_conservative = 0
+    anti_liberal = 0
+    total = 0
 
-        for tweet in data:
-            try:
-                content = tweet['text'].encode('ascii', 'ignore')
-            except:
-                continue
-            if any (word in content.lower() for word in pro_conservative_w):
-                pro_conservative += 1
-                total +=1
-            if any (word in content.lower() for word in pro_liberal_w):
-                pro_liberal += 1
-                total +=1
-            if any (word in content.lower() for word in pro_3rd_w):
-                pro_3rd += 1
-                total +=1
-            if any (word in content.lower() for word in anti_conservative_w):
-                anti_conservative += 1
-                total +=1
-            if any (word in content.lower() for word in anti_liberal_w):
-                anti_liberal += 1
-                total +=1
+    txt_val = file_name.index('_cleaned')
+    pure_file_name = file_name[:txt_val]
+    date_val = file_name.index('2016')
+    date = pure_file_name[date_val:]
+    print date
+
+    for tweet in data:
+        try:
+            content = tweet['text'].encode('ascii', 'ignore')
+        except:
+            continue
+        if any (word in content.lower() for word in pro_conservative_w):
+            pro_conservative += 1
+            total +=1
+        if any (word in content.lower() for word in pro_liberal_w):
+            pro_liberal += 1
+            total +=1
+        if any (word in content.lower() for word in pro_3rd_w):
+            pro_3rd += 1
+            total +=1
+        if any (word in content.lower() for word in anti_conservative_w):
+            anti_conservative += 1
+            total +=1
+        if any (word in content.lower() for word in anti_liberal_w):
+            anti_liberal += 1
+            total +=1
 
 
-        pro_liberal = ((pro_liberal * 1.0) / total)*100.0
-        pro_conservative = ((pro_conservative*1.0) / total)* 100.0
-        anti_liberal = ((anti_liberal*1.0) / total)* 100.0
-        anti_conservative = ((anti_conservative*1.0) / total)* 100.0
-        pro_3rd = ((pro_3rd*1.0) / total)* 100.0
-        
+    pro_liberal = ((pro_liberal * 1.0) / total)*100.0
+    pro_conservative = ((pro_conservative*1.0) / total)* 100.0
+    anti_liberal = ((anti_liberal*1.0) / total)* 100.0
+    anti_conservative = ((anti_conservative*1.0) / total)* 100.0
+    pro_3rd = ((pro_3rd*1.0) / total)* 100.0
+    
 
-        with open('tweets_sentiment_count.csv', 'a') as outputfile:
-            output = str(pro_liberal) + ',' + str(pro_conservative) + ',' + str(anti_liberal) + ',' + str(anti_conservative) + ',' + date
-            outputfile.write(output)
-            outputfile.write("\n")
-        outputfile.close()
-    for t_file in tweet_files:
-        sentize(t_file)
+    with open('tweets_sentiment_count.csv', 'a') as outputfile:
+        output = str(pro_liberal) + ',' + str(pro_conservative) + ',' + str(anti_liberal) + ',' + str(anti_conservative) + ',' + date
+        outputfile.write(output)
+        outputfile.write("\n")
+    outputfile.close()
--- main_tweet_sentiment.py
+++ main_tweet_sentiment.py
@@ -4,12 +4,12 @@ import TweetSent
 
 def main(argv):
 
-    data_file = argv[0]
+    cleaned_file = argv[0]
 
-    cleaned_file = 'cleaned' + data_file
+    # cleaned_file = 'cleaned' + data_file
     
-    jsonCleaner.json_clean(data_file, cleaned_file)
-    return TweetSent.tweet_sent(cleaned_file)
+    # jsonCleaner.json_clean(data_file, cleaned_file)
+    return TweetSent.sentize(cleaned_file)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
