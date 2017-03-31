870328e3587d795a6f1da2830cb830c66d51308d
Monica Mishra <Monica Mishra>
2017-03-31 11:04:11 -0400

    Modified retweeterDict to take multiple files

--- retweeterDict.py
+++ retweeterDict.py
@@ -1,25 +1,36 @@
 import json, datetime, unicodedata, os, sys
 
-def retweeterDict(infile):
-	dictionary = {}
-	with open(infile) as phoneFile:
-		for tweet in phoneFile:
-			try:
-				tweet = json.loads(tweet)
-			except:
-				continue
-			for t in tweet:
-				retweet = t['retweeted_status']
-				if(any(retweet)):
-					key = retweet['user']['screen_name'] + '%' + retweet['user']['user_id'] + '%' + retweet['text'] + '%' + retweet['tweet_id']
-					valueText = t['user']['screen_name'] + '%' + t['created_at'] + '%' + t['user']['location'] + '%' + t['user']['user_id']
-					value = [valueText]
-					if key in dictionary:
-						originalList = dictionary[key]
-						originalList.append(valueText)
-						dictionary[key] = originalList
-					else:
-						dictionary[key] = value
-	print dictionary
+def retweeterDict(data_file)	
+	jsonfiles = []
+	with open(data_file) as data_file:
+		for line in data_file:
+			if line != None:
+				jsonfiles.append(line.rstrip('\n'))
+	data_file.close()
 
-retweeterDict('cleanPhoneData_2017-02-19.json')
+	def retweeterDictCleaner(infile):
+		dictionary = {}
+		with open(infile) as phoneFile:
+			for tweet in phoneFile:
+				try:
+					tweet = json.loads(tweet)
+				except:
+					continue
+				for t in tweet:
+					retweet = t['retweeted_status']
+					if(any(retweet)):
+						key = retweet['user']['screen_name'] + '%' + retweet['user']['user_id'] + '%' + retweet['text'] + '%' + retweet['tweet_id']
+						valueText = t['user']['screen_name'] + '%' + t['created_at'] + '%' + t['user']['location'] + '%' + t['user']['user_id']
+						value = [valueText]
+						if key in dictionary:
+							originalList = dictionary[key]
+							originalList.append(valueText)
+							dictionary[key] = originalList
+						else:
+							dictionary[key] = value
+		print dictionary
+
+	for jsonfile in jsonfiles:
+		retweeterDictCleaner(jsonfile)
+
+# retweeterDict('') <--insert name of the file containing the json files.
