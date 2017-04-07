4ae514e0e6cbb3cbbf41f5a2a29aa8dcf0f00731
Monica Mishra <Monica Mishra>
2017-04-07 13:04:02 -0400

    amended to take in the proper files post cleaning

--- retweet_aggregator.py
+++ retweet_aggregator.py
@@ -3,35 +3,58 @@
 
 import json, datetime, unicodedata, os, sys
 
-def retweet_aggregator(data_file)
-	jsonfiles = []
-	with open(data_file) as data_file:
+def retweet_aggregator(infile, date_string):
+	data = []
+	with open(infile, 'r') as data_file:
+		i = 0
 		for line in data_file:
-			if line != None:
-				jsonfiles.append(line.rstrip('\n'))
+			# converts each line into a json object
+			data.append(json.loads(line))
+			i += 1
+	print "total tweets: " + str(i) 
 	data_file.close()
 
 	retweet_aggregator_dict = {}
+	j =0 
+	k = 0
+	for tweet in data:
+		retweet_dates = {}
+		if tweet['retweeted_status'] == {}:
+			retweet_dates[date_string] = 1
+			retweet_aggregator_dict[str(tweet["tweet_id"])] = retweet_dates
 
-	def retweet_aggregator_indiv(infile):
-		with open(infile) as phoneFile:
-			for tweet in phoneFile:
-				if tweet['retweet_subobj'] == None:
-					daily_count = {}
-					daily_count_key = 'phoneFileDay' #change this to the name of the day, dude
-					daily_count_value = 1
-					daily_count[daily_count_key] = daily_count_value
-					
-					key = tweet['tweet_id']
-					value = daily_count
-					retweet_aggregator_dict[key] = value
+		else:
+			original_tweet_id = str(tweet['retweeted_status']['tweet_id'])
+
+			if original_tweet_id in retweet_aggregator_dict:
+
+				if date_string in retweet_aggregator_dict[original_tweet_id]['retweet_dates']:
+					retweet_aggregator_dict[original_tweet_id]['retweet_dates'][date_string] += 1
+
 				else:
-					daily_count_key = 'phoneFileDay'
-					retweet_id = tweet['retweet_suboj']['tweet_id']
-					if daily_count_key in retweet_aggregator_dict[retweet_id][daily_count]:
-						retweet_aggregator_dict[retweet_id][daily_count][daily_count_key] += 1
-					else:
-						retweet_aggregator_dict[retweet_id][daily_count][daily_count_key] = 1
+					retweet_dates[date_string] = 1
+					retweet_aggregator_dict[original_tweet_id] = retweet_dates
+			
+			else:
+				retweet_dates[date_string] = 1
+				retweet_aggregator_dict[original_tweet_id] = retweet_dates
 
-	for jsonfile in jsonfiles:
-		retweet_aggregator_indiv(jsonfile)
+	print "total original tweets: " + str(j)
+	print "total retweets: " + str(k)
+        
+    #     out.append(obj)
+    # with open(outfile, 'w') as outfile:
+    #     for o in out:
+    #         outfile.write('{}\n'.format(json.dumps(o)))
+
+# works through the directory and aggregates all the cleaned files per day
+original_data_path = os.path.dirname(os.path.abspath(__file__))
+files = os.listdir(original_data_path)
+
+# Assume that original data files contain the phrase 'cleaned_phone_data'
+for n in files:
+    if 'clean_phone_data_' in n:
+        # separate out the parts of the name
+        date_string = n[17:-5]
+        print "Aggregating file: " + date_string
+        retweet_aggregator(n, date_string)
