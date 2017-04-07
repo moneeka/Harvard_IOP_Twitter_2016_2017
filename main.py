23ddaf03645e300417b8735b0cb2f4cb11f7b457
Monica Mishra <Monica Mishra>
2017-04-07 13:06:47 -0400

    added the main file for the tweet pipeline.

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import deduplicate_phone_data
+import twitter_phone_numbers
+import retweet_aggregator
+
+def main(argv):
+
+	#As a first step, we merge all the twitter files from the same day and delete the duplicates. The new file name will be "mergedPhoneData_[insert date]".
+	#For example, mergedPhoneData_2017_03_06.json
+	deduplicate_phone_data
+	twitter_phone_numbers
+	retweet_aggregator
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
