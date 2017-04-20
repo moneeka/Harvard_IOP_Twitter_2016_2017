e9b074d5cfb63019bd8be2bc83fbb1cb89f0bb52
Matt Leifer <matthewleifer@college.harvard.edu>
2017-04-20 13:53:26 -0400

    actually added day counts

---
+++ hashtag_day_counts_working.py
@@ -0,0 +1,30 @@
+import cPickle, os, operator
+
+hashtag_date_counts = {}
+
+data_path = os.path.dirname(os.path.abspath(__file__)) + "/TagCounts"
+data_files = os.listdir(data_path)
+
+for doc_name in data_files:
+	doc = open("TagCounts/"+doc_name, 'r')
+	l = cPickle.load(doc)
+	date = doc_name.split('tag')[1].split('.pkl')[0]
+	for line in l:
+		hashtag = line[0].lower()
+
+		if hashtag in hashtag_date_counts:
+			if date in hashtag_date_counts[hashtag]:
+				hashtag_date_counts[hashtag][date] += line[1]
+			else:
+				hashtag_date_counts[hashtag][date] = line[1]
+		else:
+			hashtag_date_counts[hashtag] = {}
+			hashtag_date_counts[hashtag][date] = line[1]
+	doc.close()
+
+#print hashtag_date_counts
+
+sorted_x = sorted(hashtag_date_counts.items(), key=operator.itemgetter(1))
+print hashtag_date_counts["trump"]
+for k,v in hashtag_date_counts.items():
+	print k,v
